8ae904492ee24240179d8998184cfcab4334af5a
Ansh Tripathi <tripathiansh57@gmail.com>
2023-05-14 18:14:27 +0530

    initial commit

---
+++ src/backend/db/products.js
@@ -0,0 +1,155 @@
+import { v4 as uuid } from "uuid";
+
+/**
+ * Product Database can be added here.
+ * You can add products of your wish with different attributes
+ * */
+
+export const products = [
+  {
+    _id: uuid(),
+    title: "Men Printed Round Neck Black T-Shirt",
+    descriptions: "",
+    originalPrice: 789,
+    price: 500,
+    categoryName: "men",
+    trending: true,
+    image:
+      "https://rukminim1.flixcart.com/image/832/832/xif0q/t-shirt/t/e/0/l-st-theboys-black-smartees-original-imagnqszzzzyuzru.jpeg?q=70",
+  },
+  {
+    _id: uuid(),
+    title: "Women Typography T-Shirt",
+    descriptions: "Women Typography Round Neck Black T-Shirt",
+    originalPrice: 799,
+    price: 540,
+    categoryName: "women",
+    image:
+      "https://rukminim1.flixcart.com/image/832/832/xif0q/t-shirt/b/w/7/s-ww-smile-black-smartees-original-imagnxamfkh2jnrt.jpeg?q=70",
+  },
+  {
+    _id: uuid(),
+    title: "Men Printed T-Shirt",
+    descriptions: "Men Printed, Typography Round Neck Black T-Shirt",
+    originalPrice: 889,
+    price: 600,
+    categoryName: "men",
+    image:
+      "https://rukminim1.flixcart.com/image/832/832/xif0q/t-shirt/m/o/0/xl-nevergiveup-black-halfsleeve-smartees-original-imaghqfap26k5xbu.jpeg?q=70",
+  },
+  {
+    _id: uuid(),
+    title: "Women Typography T-Shirt",
+    descriptions: "Women Typography Round Neck Black T-Shirt",
+    originalPrice: 999,
+    price: 670,
+    categoryName: "women",
+    trending: true,
+    image:
+      "https://rukminim1.flixcart.com/image/832/832/xif0q/t-shirt/b/w/7/s-ww-smile-black-smartees-original-imagnxamfkh2jnrt.jpeg?q=70",
+  },
+  {
+    _id: uuid(),
+    title: "Women Anarkali Kurta",
+    descriptions:
+      "Women Floral Print Viscose Rayon Anarkali Kurta  (Dark Blue)",
+    originalPrice: 749,
+    price: 570,
+    categoryName: "women",
+    image:
+      "https://rukminim1.flixcart.com/image/832/832/kzk6bgw0/kurta/7/j/m/xxl-kurta-flared-u-neck-013-dhakre-fashion-original-imagbjgjxgb3pmbx.jpeg?q=70",
+  },
+  {
+    _id: uuid(),
+    title: "Ausk Polo T-Shirt for Men",
+    descriptions: "Polo T-shirt with Welt Pocket",
+    originalPrice: 567,
+    price: 380,
+    categoryName: "men",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmxwNlOTvz7QWULnCec2dVjTnndXya65oVcg&usqp=CAU",
+  },
+  {
+    _id: uuid(),
+    title: "Roadster T-Shirt for Men",
+    descriptions: "Polo T-shirt with Welt Pocket",
+    originalPrice: 789,
+    price: 350,
+    trending: true,
+    categoryName: "men",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmxwNlOTvz7QWULnCec2dVjTnndXya65oVcg&usqp=CAU",
+  },
+  {
+    _id: uuid(),
+    title: "BLive",
+    descriptions: "Boys Solid Cotton Blend T Shirt  (Dark Blue, Pack of 1)",
+    originalPrice: 1999,
+    price: 617,
+    categoryName: "kids",
+    image:
+      "https://rukminim1.flixcart.com/image/832/832/xif0q/kids-t-shirt/c/m/c/14-15-years-bnvpoloboy-plain-blive-original-imagmeyyeqpf2txh.jpeg?q=70",
+  },
+  {
+    _id: uuid(),
+    title: "Fubar Shirt for Men",
+    descriptions: "Men Slim Fit Solid Spread Collar Formal Shirt",
+    originalPrice: 789,
+    price: 419,
+    categoryName: "men",
+    image:
+      "https://rukminim1.flixcart.com/image/832/832/xif0q/shirt/x/6/q/m-kcsh-fo-1647-ma-fubar-original-imaghg9qdht3ghgn.jpeg?q=70",
+  },
+  {
+    _id: uuid(),
+    title: "Women Typography T-Shirt",
+    descriptions: "Women Typography Round Neck Black T-Shirt",
+    originalPrice: 589,
+    price: 300,
+    categoryName: "women",
+    image:
+      "https://rukminim1.flixcart.com/image/832/832/kzk6bgw0/kurta/7/j/m/xxl-kurta-flared-u-neck-013-dhakre-fashion-original-imagbjgjxgb3pmbx.jpeg?q=70",
+  },
+  {
+    _id: uuid(),
+    title: "Nike shoes for Men",
+    descriptions: "Revolution 5 Running Shoes For Men  (Black)",
+    originalPrice: 2999,
+    price: 2014,
+    categoryName: "men",
+    image:
+      "https://rukminim1.flixcart.com/image/832/832/xif0q/shoe/a/b/t/-original-imagpgpekpzwjchk.jpeg?q=70",
+  },
+  {
+    _id: uuid(),
+    title: "Alan Jones",
+    descriptions: "Boys Striped Cotton Blend T Shirt  (Dark Green, Pack of 1)",
+    originalPrice: 999,
+    price: 386,
+    categoryName: "kids",
+    image:
+      "https://rukminim1.flixcart.com/image/832/832/l05lx8w0/kids-t-shirt/s/x/d/15-16-years-boy22-pt01-bgrn-alan-jones-original-imagcyfrjg2mhnyj.jpeg?q=70",
+  },
+
+  {
+    _id: uuid(),
+    title: "Print My Fashion",
+    descriptions:
+      "Boys Graphic Print Polyester T Shirt  (Dark Blue, Pack of 1)",
+    originalPrice: 1200,
+    price: 599,
+    categoryName: "kids",
+    image:
+      "https://rukminim1.flixcart.com/image/832/832/kvzkosw0/kids-t-shirt/x/0/b/5-6-years-pmf-ts-b-p6-nb-5-6y-print-my-fashion-original-imag8rufzvjvhnzb.jpeg?q=70",
+  },
+  {
+    _id: uuid(),
+    title: "NIKE T-Shirt for Men",
+    descriptions: "Men Solid Round Neck Blue T-Shirt",
+    originalPrice: 1595,
+    price: 1152,
+    categoryName: "men",
+    image:
+      "https://rukminim1.flixcart.com/image/832/832/xif0q/t-shirt/s/w/7/m-dv2322-469-nike-original-imaghhzzp9zdhgdx.jpeg?q=70",
+  },
+];
